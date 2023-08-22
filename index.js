9525190bd49b0d8d789c4553ed6528c4fc223eca
Ricardo Prezotto <48772496+Prezt@users.noreply.github.com>
2023-08-22 17:15:24 -0300

    implements remainder functions

--- index.js
+++ index.js
@@ -2,8 +2,88 @@ import * as fs from 'fs';
 import csv from 'csv-parser'
 import Intl  from 'intl'
 
+const validateDigitFromSum = (validSum, validDig) => {
+    const divisionRemainder = validSum % 11;
+    const expectedDig = divisionRemainder < 2 ? 0 : 11 - divisionRemainder;
+    return expectedDig === validDig;
+}
+
+const validateVlPresta = (vlTotal, qtPrestacoes, vlPresta) => {
+    const expectedVlPresta = Math.round(Number(vlTotal)/Number(qtPrestacoes)*100)/100;
+    return expectedVlPresta === Number(vlPresta);
+}
+
+const validateCpfCnpj = (documentNumber) => {
+    if(documentNumber.length === 11){
+        // CPF
+        const numArray = documentNumber.split('').map((el) => Number(el));
+        const validationDigit2 = numArray.pop();
+        const validationDigit1 = numArray.pop();
+        numArray.reverse();
+        let validSum = 0;
+        for(let i=0; i< numArray.length; i++){
+            validSum += numArray[i] * (i+2);
+        }
+        if(!validateDigitFromSum(validSum, validationDigit1)){
+            // FALHOU A PRIMEIRA VALIDAÇÃO - esse return poderia estar ao final do código por motivos 
+            // de clareza, mas apenas por eficiencia é melhor que fique aqui, pois remove validações desnecessárias
+             return false
+        } 
+
+        numArray.reverse().push(validationDigit1)
+        numArray.reverse();
+        validSum = 0;
+        for(let i=0; i< numArray.length; i++){
+            validSum += numArray[i] * (i+2);
+        }
+        if(!validateDigitFromSum(validSum, validationDigit2)){
+            return false
+        }
+        return true
+    } else if(documentNumber.length === 14) {
+        // CNPJ
+        const numArray = documentNumber.split('').map((el) => Number(el));
+        const validationDigit2 = numArray.pop();
+        const validationDigit1 = numArray.pop();
+        const multipliers = [5,4,3,2,9,8,7,6,5,4,3,2];
+        let validationSum = 0;
+        for(let i=0; i < numArray.length; i++){
+            validationSum += numArray[i] * multipliers[i];
+        }
+        if(!validateDigitFromSum(validationSum, validationDigit1)){
+            // FALHOU A PRIMEIRA VALIDAÇÃO - esse return poderia estar ao final do código por motivos 
+            // de clareza, mas apenas por eficiencia é melhor que fique aqui, pois remove validações desnecessárias
+             return false
+        }
+        numArray.push(validationDigit1);
+        multipliers.reverse().push(6);
+        multipliers.reverse();
+        validationSum = 0;
+        for(let i=0; i < numArray.length; i++){
+            validationSum += numArray[i] * multipliers[i];
+        }
+        if(!validateDigitFromSum(validationSum, validationDigit2)){
+            return false
+        }
+        return true
+    }
+    return false
+}
+
 const fomatBRL = (currencyValue) =>{
     return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(currencyValue)
+}
+
+const formatDate = (dateString) => {
+    const dateStringArr = dateString.split('');
+    const y = dateStringArr.slice(0,4);
+    const m = dateStringArr.slice(4,6);
+    const d = dateStringArr.slice(6,8);
+
+    const year = Number(y.join(''));
+    const month = Number(m.join('')) - 1;
+    const day = Number(d.join(''));
+    return new Date(year, month, day);
 }
 
 const parseCSV = (filePath) => {
@@ -12,13 +92,18 @@ const parseCSV = (filePath) => {
         fs.createReadStream(filePath)
         .pipe(csv())
         .on('data', (data)=> {
+            const {nrCpfCnpj, vlTotal, qtPrestacoes, vlPresta, vlMora, vlMulta, vlAtual, dtContrato, dtVctPre} = data;
+            const validDocument = validateCpfCnpj(nrCpfCnpj);
+            const validVlPresta = validateVlPresta(vlTotal,qtPrestacoes,vlPresta);
             const formattedData = {
                 ...data,
-                vlTotal: fomatBRL(data.vlTotal),
-                vlPresta: fomatBRL(data.vlPresta),
-                vlMora: fomatBRL(data.vlMora),
-                vlMulta: fomatBRL(data.vlMulta),
-                vlAtual: fomatBRL(data.vlAtual),
+                dtContrato: formatDate(dtContrato),
+                dtContrato: formatDate(dtVctPre),
+                vlTotal: fomatBRL(vlTotal),
+                vlPresta: fomatBRL(vlPresta),
+                vlMora: fomatBRL(vlMora),
+                vlMulta: fomatBRL(vlMulta),
+                vlAtual: fomatBRL(vlAtual),
             } 
             csvData.push(formattedData);
         })
@@ -35,7 +120,7 @@ const parseCSV = (filePath) => {
 const main = async () => {
     const filePath = './data.csv'
     const csvData = await parseCSV(filePath);
-    console.log(csvData[0])
+    console.log(csvData.length)
     
 }
 
