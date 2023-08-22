1ca0375da593abae7e1e292f45c4b439d9e9c1fc
Matheus Henrique Sousa Brito <matheus.henriquesousabrito@einstein.br>
2023-08-22 12:49:09 -0300

    feat: created some cases tests like verify BRL and verify CPF and CNPJ valid

--- test/utils.js
+++ test/utils.js
@@ -1,18 +1,51 @@
 import {
     describe,
-    it,
-    mock
+    it
 } from 'node:test'
 import assert from 'node:assert'
-import { validNrCpfCnpj, CSVRead, validatePrestationCalculation, brlFormatter, convertToDate } from "../utils.js"
+import { CSVRead, validNrCpfCnpj } from "../utils.js"
+import { transformData } from "../transformData.js"
+ 
+describe('TransformData', () => {
+ 
+    it('should verify R$', async () => {
+        const results = [];
+        await CSVRead(
+            (data) => results.push(data),
+            () => {
+                const [{ vlTotal, vlPresta, vlMora }] = transformData(results);
 
-
-
-describe('Utils', () => {
-
-    it('should verify brlFormatter', () => {
-
-        console.log(brlFormatter)
+                assert.strictEqual(vlTotal.includes('R$'), true)
+                assert.strictEqual(vlPresta.includes('R$'), true)
+                assert.strictEqual(vlMora.includes('R$'), true)
+            }
+        )
     })
+
+
+    it('should verify CPF and CNPJ', async () => {
+        const validCpfMock = validNrCpfCnpj(`85581411000`)
+        const validCnpjMock = validNrCpfCnpj(`26527051000160`)
+
+        assert.strictEqual(validCpfMock.isValidCpf, true)
+        assert.strictEqual(validCnpjMock.isValidCnpj, true)
+    })
+
+    it('should verify vlPresta', async () => {
+        const results = [];
+        await CSVRead(
+            (data) => results.push(data),
+            () => {
+                const [{ vlPresta, vlPrestaConsistentes }] = transformData(results);
+ 
+                assert.equal(vlPrestaConsistentes, false)
+
+
+            }
+        )
+    })
+
+
+
 
 })
