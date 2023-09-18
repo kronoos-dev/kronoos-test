7dbce458e25fb06de75b5c58a941401c4893c5ff
ronaldocaldeireirojonson <ronaldooscarcaldeireirojonson@gmail.com>
2023-09-18 17:24:47 -0300

    read me and minor fix

--- Utils/currency.js
+++ Utils/currency.js
@@ -1,11 +1,13 @@
 
+const formater = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+})
+
 function convertToBRL(value){
-    return new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL',
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-    }).format(value);
+    return formater.format(value);
 }
 
 
