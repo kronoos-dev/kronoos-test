45dfa956b2b1edbf046bb0df5a443aa37ef38c7d
Abilio Bittencourt <abilio.jr@live.com>
2023-12-19 13:05:39 -0300

    refactor: unnecessary function

--- src/infrastructure/files/read-file.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-
-const fspromise = fs.promises
-const readFile = async (filePath: string): Promise<string> => {
-    return await fspromise.readFile(filePath, 'utf8');
-}
-
-export default readFile
