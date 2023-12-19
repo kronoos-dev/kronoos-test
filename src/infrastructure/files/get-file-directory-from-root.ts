7e1bfd4aacb69d0b7e2889168da2272dc3860268
Abilio Bittencourt <abilio.jr@live.com>
2023-12-19 12:01:57 -0300

    feat: get file directory from root

---
+++ src/infrastructure/files/get-file-directory-from-root.ts
@@ -0,0 +1,10 @@
+import fs from 'fs';
+import path from 'path'
+
+
+const getFileDirectoryFromRoot = (fileName: string): string => {
+    const pathJoin = path.join(`${__dirname}\\..\\..\\..\\${fileName}`)
+    return pathJoin;
+}
+
+export default getFileDirectoryFromRoot
