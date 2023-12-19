d77d8b11f3a55d985431b17eded933edbec5ee91
Samuel Morrissey <samuel.morrissey@outlook.com>
2023-12-19 11:49:22 -0300

    Configurações iniciais do projeto

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log("Aplicativo rodando na porta 3000");
+});
