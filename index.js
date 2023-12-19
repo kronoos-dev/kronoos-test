ebd4b7105267106629ae7ef274f89e831bbd18f0
Wilson Fernando Pena <wilsonf.pena@gmail.com>
2023-12-19 07:25:38 -0300

    feat: process csv file validate and convert necessary properties and save JSON

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const csv = require('csv-parser');
 const fs = require('fs');
+const { converters, validators } = require('./utils');
+
+const validateDocument = ({ nrCpfCnpj }) => {
+	if (validators.isValidCPF(nrCpfCnpj)) {
+		return { tpDocumento: 'CPF' };
+	}
+	if (validators.isValidCNPJ(nrCpfCnpj)) {
+		return { tpDocumento: 'CNPJ' };
+	}
+	return { tpDocumento: 'Documento Invalido' };
+};
 
 const run = (file = 'data.csv') => {
 	const results = [];
@@ -8,10 +19,20 @@ const run = (file = 'data.csv') => {
 	fs.createReadStream(file)
 		.pipe(csv())
 		.on('data', (data) => {
-			results.push({ ...data });
+			const prestacaoValida = validators.hasValidPrestacoes(data);
+			results.push({
+				...data,
+				...validateDocument(data),
+				vlTotal: converters.convertToBRL(data.vlTotal),
+				vlPresta: converters.convertToBRL(data.vlPresta),
+				vlMora: converters.convertToBRL(data.vlMora),
+				dtVctPre: converters.convertToDate(data.dtVctPre),
+				dtContrato: converters.convertToDate(data.dtContrato),
+				prestacaoValida,
+			});
 		})
 		.on('end', () => {
-			console.log(`Finished processing ${results.length} rows. Saving JSON to file...`);
+			console.log(`Finished processing. Saving JSON to file...`);
 			fs.writeFileSync(resultFilePath, JSON.stringify(results, null, 2));
 			console.log(`Finished processing ${file} saved JSON array to ${resultFilePath}`);
 		});
