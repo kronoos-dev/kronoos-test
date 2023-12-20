0fe5c7d85072ba3bf2aa9a2c3f68bad60e70472d
Samuel Morrissey <samuel.morrissey@outlook.com>
2023-12-20 12:55:55 -0300

    Ajuste de alinhamento

--- frontend/src/page.tsx
+++ frontend/src/page.tsx
@@ -3,10 +3,10 @@ import DataTable from "./components/data-table";
 export default function HomePage() {
   return (
     <div className="overflow-hiddenpy-5 flex h-screen flex-col items-center justify-start gap-4">
-      <header>
+      <header className="flex flex-col items-center">
         <h1 className="text-center text-4xl">Kronoos Test</h1>
         <p>Frontend proposto para teste das funcionalidades</p>
-        <span>by: Samuel Morrissey</span>
+        <span>by Samuel Morrissey</span>
       </header>
       <main className="h-full w-full overflow-hidden px-20">
         <DataTable />
