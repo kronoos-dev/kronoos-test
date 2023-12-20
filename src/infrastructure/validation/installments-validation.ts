9d71e9053972ee7ca388a33022f880cf0c365b8e
Abilio Bittencourt <abilio.jr@live.com>
2023-12-20 00:07:59 -0300

    refactor: add condition to avoid error with number of installments

--- src/infrastructure/validation/installments-validation.ts
+++ src/infrastructure/validation/installments-validation.ts
@@ -1,8 +1,11 @@
 const intallmentsValidation = (totalString: string, installmentsString: string, installmentValueString: string) =>{
+    const total = Number(totalString);
+    const installments = Number(installmentsString);
+    const installmentValue = Number(installmentValueString);
+    if(!Number.isInteger(installments)) {
+        return false
+    }
     if(totalString && installmentsString && installmentValueString) {
-        const total = Number(totalString);
-        const installments = Number(installmentsString);
-        const installmentValue = Number(installmentValueString);
         const installmentNumber = Number(Number(total / installments).toFixed(2));
 
         return installmentNumber === installmentValue;
